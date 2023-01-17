2ce677300028beb09f55f7d12b46c9a9ec8cadbf
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-17 19:25:44 +0200

    add

--- lesson_2/jsx_render_elements/webpack.config.js
+++ lesson_2/jsx_render_elements/webpack.config.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => {
