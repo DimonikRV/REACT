3ab604bf34fff917d709fd3972a907c29932087a
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-17 11:42:03 +0200

    add

--- lesson_1/first-react-app-with-jsx/src/index.js
+++ lesson_1/first-react-app-with-jsx/src/index.js
@@ -12,21 +12,3 @@ const element = (
 );
 
 ReactDOM.render(element, rootElement);
-// const renderGreeting = (elem) => {
-//   const containerElem = document.createElement("div");
-//   containerElem.classList.add("greeting");
-
-//   const titleElem = document.createElement("h1");
-//   titleElem.classList.add("greeting__title");
-//   titleElem.textContent = "Hello, world!";
-
-//   const textElem = document.createElement("p");
-//   textElem.classList.add("greeting__text");
-//   textElem.textContent = "I am learning React";
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
