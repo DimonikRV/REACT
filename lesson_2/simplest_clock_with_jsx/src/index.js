e2ff9ec5c2f9d60008f28f25228e52280b0369fb
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-18 15:08:13 +0200

    add

---
+++ lesson_2/simplest_clock_with_jsx/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElem = document.getElementById('root');
+
+const timerRender = time => {
+  const seconds = new Date(time).getSeconds();
+
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const texstColor = seconds % 2 !== 0 ? '#fff' : '#000';
+  const styles = {
+    backgroundColor,
+    color: texstColor,
+  };
+
+  const element = (
+    <div classNane="seconds" style={styles}>
+      {seconds}
+    </div>
+  );
+  ReactDOM.render(element, rootElem);
+};
+const setTimer = setInterval(timerRender(new Date()), 1000);
