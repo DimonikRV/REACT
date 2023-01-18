5b18fd1c298f5d8336409a9c5e1328a4b7534334
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-18 18:43:36 +0200

    add

--- lesson_2/todo_list_2/src/index.js
+++ lesson_2/todo_list_2/src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
-import 'styles.css';
+import './index.css';
+
+const rootElem = document.getElementById('root');
+
+const element = (
+  <>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+      </div>
+      <ul className="list"></ul>
+    </main>
+  </>
+);
+ReactDOM.render(element, rootElem);
