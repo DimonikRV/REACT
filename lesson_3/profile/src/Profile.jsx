025ef6b8b7f34dc3c0d1578adbc4b7f4922b4e21
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-20 21:46:55 +0200

    add

--- lesson_3/profile/src/Profile.jsx
+++ lesson_3/profile/src/Profile.jsx
@@ -8,7 +8,7 @@ function Profile(props) {
   return (
     <div className="profile">
       <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
-      <div className="profile__birth">{`Was born${formatDate(props.userData.birthDate)} in ${
+      <div className="profile__birth">{`Was born ${formatDate(props.userData.birthDate)} in ${
         props.userData.birthPlace
       }`}</div>
     </div>
