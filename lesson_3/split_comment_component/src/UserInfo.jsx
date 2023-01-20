8cc2b0edccce4320e6761d04e8816c4f62389cb2
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-20 14:46:53 +0200

    add

--- lesson_3/split_comment_component/src/UserInfo.jsx
+++ lesson_3/split_comment_component/src/UserInfo.jsx
@@ -5,7 +5,7 @@ import './user_info.scss';
 function UserInfo(props) {
   return (
     <div className="user-info">
-      <Avatar name={props.author.name} userUrl={props.user.avatarUrl} />
+      <Avatar name={props.user.name} userUrl={props.user.avatarUrl} />
       <div className="user-info__name">{props.user.name}</div>
     </div>
   );
