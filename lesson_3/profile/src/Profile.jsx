1007322d9aa5c9767a0271abb55c6ce1fbd7560c
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-20 21:45:35 +0200

    add

--- lesson_3/profile/src/Profile.jsx
+++ lesson_3/profile/src/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import './profile.scss';
 
-const formatDate = date => moment(date).format('DD MMM YYYY');
+const formatDate = date => moment(date).format('DD MMM YY');
 
 function Profile(props) {
   return (
