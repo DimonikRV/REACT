9740c911185b43210784ff99edf51739af0b0bfb
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-24 15:46:24 +0200

    add

--- lesson_6/conditional_rendering_with_if/src/UserGreeting.jsx
+++ lesson_6/conditional_rendering_with_if/src/UserGreeting.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const UserGreeting = () => {
-    return <h1>Hello, user</h1>
-}
+  return <h1>Hello, user 😎</h1>;
+};
 
-export default UserGreeting
+export default UserGreeting;
