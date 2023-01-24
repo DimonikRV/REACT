5dc20096cb6be62f70ee8e10284d4f1a9aaa6d75
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-24 22:23:43 +0200

    add

---
+++ lesson_6/info_about_products/src/Page.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Info from './Info';
+
+const text1 = 'Price is 500$. Available in 2 colors';
+const text2 = 'Price is 650$. Not available';
+
+class Page extends React.Component {
+  state = {
+    text: null,
+  };
+
+  SetText = text => {
+    this.setState({
+      text,
+    });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <Info info={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.SetText(text1)}>
+            IPhone 13
+          </button>
+          <button className="btn" onClick={() => this.SetText(text2)}>
+            IPhone 13 Pro
+          </button>
+          <button className="btn" onClick={() => this.SetText('')}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Page;
