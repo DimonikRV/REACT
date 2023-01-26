39831f1dc91ddbd8367602cab206c79acd3e167c
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-26 16:55:48 +0200

    add

---
+++ lesson_7/users_list_with_key/src/UserList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import User from "./User";
+
+class UserList extends React.Component {
+
+    state = {
+        sorting: null,
+    }
+
+    toggleSorting = () => {
+        const newSorting = this.state.sorting === 'asc'
+            ? 'desc'
+            : 'asc';
+        this.setState({
+            sorting: newSorting,
+    })
+    }
+
+  render() {
+    let UsersList;
+        if (this.state.sorting) {
+             UsersList = this.props.users.sort((a, b) => this.state.sorting === "asc"
+              ? a.age - b.age
+              : b.age - a.age)
+        } else {
+          UsersList = this.props.users;
+        }
+      
+        return (
+            <div>
+                <button className="btn" onClick={this.toggleSorting}>{ this.state.sorting ||'-'}</button>
+                <ul className="users">
+                      {UsersList.map(user => <User key={user.id} {...user} />)}
+                </ul>
+            </div>
+
+        )
+    }
+}
+export default UserList; 
