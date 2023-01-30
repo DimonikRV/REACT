7ab82ef5e9e8d46069d41f5076bfde4faa987869
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-30 13:06:17 +0200

    add

---
+++ lesson_9/controlled_component/src/Search.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Search extends React.Component {
+  state = {
+    value: '',
+  };
+  onChangeHandler = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  onSubmit = event => {
+    event.preventDefault();
+    alert(event.target.value);
+  };
+  render() {
+    return (
+      <form className="search" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="search__input"
+          value={this.state.value}
+          onChange={this.onChangeHandler}
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+export default Search;
