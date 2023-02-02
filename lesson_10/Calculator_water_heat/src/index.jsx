eaddfcf34b146dd6d49424bd5f57b4aec527150d
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-02-02 20:08:54 +0200

    add

---
+++ lesson_10/Calculator_water_heat/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator';
+
+
+const rootElem = document.getElementById('root');
+
+ReactDOM.render(<Calculator />, rootElem);
