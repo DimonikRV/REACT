18eb34b4a4c6532f98f66ba8dd59da857242e178
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-02-04 16:04:07 +0200

    add

--- lesson_update_component/task3_expanded_component/src/Expand.jsx
+++ lesson_update_component/task3_expanded_component/src/Expand.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 class Expand extends React.Component {
   render() {
