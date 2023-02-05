bf888f6b6ce0601dcbfd19d0f01605ce2cbfc679
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-02-05 11:44:23 +0200

    add

--- lesson_update_component/task3_expanded_component/src/Expand.jsx
+++ lesson_update_component/task3_expanded_component/src/Expand.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Expand extends React.Component {
+  state = {
+    isVisible: false,
+  };
+  onToggler = () => {
+    this.setState({
+      isVisible: !this.state.isVisible,
+    });
+  };
+
   render() {
-    const { children, title, onToggler, isVisible } = this.props;
+    const { isVisible } = this.state;
+    const { children, title } = this.props;
     return (
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">{title}</span>
-          <button className="expand__toggle-btn" onClick={onToggler}>
-            <i className={`fas fa-chevron-${isVisible ? 'up' : 'down'}`} />
+          <button className="expand__toggle-btn" onClick={this.onToggler}>
+            {isVisible ? (
+              <i className="fas fa-chevron-up"></i>
+            ) : (
+              <i className="fas fa-chevron-down"></i>
+            )}
           </button>
         </div>
         {isVisible && <div className="expand__content">{children}</div>}
@@ -20,8 +34,6 @@ class Expand extends React.Component {
 Expand.propTypes = {
   children: PropTypes.element.isRequired,
   title: PropTypes.string,
-  onToggler: PropTypes.func.isRequired,
-  isVisible: PropTypes.bool.isRequired,
 };
 
 Expand.defaultProps = {
