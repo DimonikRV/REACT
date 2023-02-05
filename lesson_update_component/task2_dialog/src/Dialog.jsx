039ca4b5ae3e65b81ec4763777f14cc1ec0c1461
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-02-05 12:16:20 +0200

    add

--- lesson_update_component/task2_dialog/src/Dialog.jsx
+++ lesson_update_component/task2_dialog/src/Dialog.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Dialog extends React.Component {
-  render() {
-    const { children, isOpen, onClose, title } = this.props;
-    if (!isOpen) {
-      return null;
-    }
-    return (
-      <div class="dialog">
-        <div class="dialog__heading">
-          <h4 class="dialog__title">{title}</h4>
-          <button class="dialog__close-btn" onClick={onClose}>
-            +
-          </button>
-        </div>
-        <div class="dialog__content">{children}</div>
-      </div>
-    );
+const Dialog = ({ children, isOpen, onClose, title }) => {
+  if (!isOpen) {
+    return null;
   }
-}
+  return (
+    <div class="dialog">
+      <div class="dialog__heading">
+        <h4 class="dialog__title">{title}</h4>
+        <button class="dialog__close-btn" onClick={onClose}>
+          +
+        </button>
+      </div>
+      <div class="dialog__content">{children}</div>
+    </div>
+  );
+};
 Dialog.propTypes = {
   children: PropTypes.element.isRequired,
   isOpen: PropTypes.bool,
