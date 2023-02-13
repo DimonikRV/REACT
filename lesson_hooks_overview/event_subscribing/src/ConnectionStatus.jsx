9d59d0de36a2ab1f956083a4167b374a7441ec87
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-02-13 17:16:18 +0200

    add

---
+++ lesson_hooks_overview/event_subscribing/src/ConnectionStatus.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+
+const ConnectionStatus = () => {
+  const [connection, setConnection] = useState(true);
+
+  useEffect(() => {
+    const handleOnline = event => {
+      const isOnline = event.target.navigator.isOnline;
+      setConnection(isOnline);
+    };
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOnline);
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOnline);
+    };
+  });
+
+  const statusElem = classNames('status', { status_offline: !connection });
+  const statusTxt = connection ? 'online' : 'offline';
+  return <div className={statusElem}>{statusTxt}</div>;
+};
+
+export default ConnectionStatus;
