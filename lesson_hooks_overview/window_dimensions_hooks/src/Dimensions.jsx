dfbfe97d45b99923a01ed5b2825b02253bdd2ed9
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-02-13 15:36:22 +0200

    add

---
+++ lesson_hooks_overview/window_dimensions_hooks/src/Dimensions.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+const Dimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    width: '',
+    height: '',
+  });
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    setDimensions({
+      width: innerWidth,
+      height: innerHeight,
+    });
+    const onResizeHandler = event => {
+      const { innerWidth, innerHeight } = event.target;
+      setDimensions({
+        width: innerWidth,
+        height: innerHeight,
+      });
+    };
+    window.addEventListener('resize', onResizeHandler);
+    return () => {
+      window.removeEventListener('resize', onResizeHandler);
+    };
+  }, []);
+  const { width, height } = dimensions;
+  return <div className="dimensions">{`${width} - ${height}`}</div>;
+};
+
+export default Dimensions;
